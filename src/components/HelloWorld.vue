b6efa53eb45c17c64bf46cad78f640bc0073ec13
hefengyuan <hefengyuan@192.168.1.6>
2026-06-03 23:51:03 +0800

    first部署

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+// import { ref } from 'vue'
 import viteLogo from '../assets/vite.svg'
 import heroImg from '../assets/hero.png'
 import vueLogo from '../assets/vue.svg'
