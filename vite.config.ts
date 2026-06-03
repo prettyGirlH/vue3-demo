9f87b8258e6a3e47f01c33afda5fd6283e2b6b0b
hefengyuan <hefengyuan@192.168.1.6>
2026-06-03 23:55:31 +0800

    first部署

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vue3-demo/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
